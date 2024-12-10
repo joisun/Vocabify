c29de697b63d4d96eaddacaecaa55c530c064112
joi@rog <joi-sun@outlook.com>
2024-12-10 18:55:36 +0800

    feat: MockLoading component

---
+++ components/custom/MockLoading.tsx
@@ -0,0 +1,28 @@
+import { Loader } from "lucide-react";
+
+export default function MockLoading() {
+  const [loading, setLoading] = useState(true);
+  const loadingRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+      clearTimeout(timer);
+      const timer2 = setTimeout(() => {
+        loadingRef.current && loadingRef.current.remove();
+        clearTimeout(timer2);
+      }, 1000);
+    }, 800);
+  }, []);
+  return (
+    <div
+      ref={loadingRef}
+      className="container transition-all duration-1000 mx-auto max-w-4xl p-6 bg-background z-10 flex flex-col justify-center items-center h-screen absolute inset-0"
+      style={{ opacity: loading ? 1 : 0 }}
+    >
+      <h1 className="mb-6 text-4xl font-semibold">Vocabify</h1>
+      <div className="animate-spin">
+        <Loader className="animate-scaleUp" />
+      </div>
+    </div>
+  );
+}
