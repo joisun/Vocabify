7ea2b105f2433b28addada90ce25fb989476431a
joi@rog <joi-sun@outlook.com>
2024-12-11 16:46:09 +0800

    feat: style for content

--- entrypoints/content/components/TooltipBtn.tsx
+++ entrypoints/content/components/TooltipBtn.tsx
@@ -1,26 +1,41 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator"
-import { CopyIcon, XIcon, Aperture } from "lucide-react"
-import { copyHandler } from "../utils";
-function TooltipBtn({ text, cancelHandler, vocabifyHandler }: { text: string, vocabifyHandler: (text: string) => void, cancelHandler: (text: string) => void }) {
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { CopyIcon, XIcon, Aperture } from 'lucide-react'
+import { copyHandler } from '../utils'
+function TooltipBtn({
+  text,
+  cancelHandler,
+  vocabifyHandler,
+}: {
+  text: string
+  vocabifyHandler: (text: string) => void
+  cancelHandler: (text: string) => void
+}) {
   return (
     <TooltipProvider>
       <Tooltip open>
-        <TooltipTrigger className="" asChild><span>{text}</span></TooltipTrigger>
-        <TooltipContent className="flex space-x-1 text-sm">
-          <Button className="bg-white/20" size={"sm"} variant="ghost" onClick={() => vocabifyHandler(text)}><Aperture />Vocabify</Button> <Separator orientation="vertical" />
-          <Button className="bg-white/20" size={"sm"} variant="ghost" onClick={() => copyHandler(text)}><CopyIcon />Copy</Button> <Separator orientation="vertical" />
-          <Button size={"sm"} variant="destructive" onClick={() => cancelHandler(text)}><XIcon /></Button>
+        <TooltipTrigger className="" asChild>
+          <span>{text}</span>
+        </TooltipTrigger>
+        <TooltipContent className="flex space-x-1 text-sm rounded-full p-1">
+          <Button className="rounded-full h-7 bg-white/20" size={'sm'} variant="ghost" onClick={() => vocabifyHandler(text)}>
+            <Aperture />
+            Vocabify
+          </Button>{' '}
+          <Separator orientation="vertical" />
+          <Button className="rounded-full h-7 bg-white/20" size={'sm'} variant="ghost" onClick={() => copyHandler(text)}>
+            <CopyIcon />
+            Copy
+          </Button>{' '}
+          <Separator orientation="vertical" />
+          <Button className="rounded-full h-7 w-7" size={'icon'} variant="destructive" onClick={() => cancelHandler(text)}>
+            <XIcon />
+          </Button>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
-  );
+  )
 }
 
-export default TooltipBtn;
+export default TooltipBtn
