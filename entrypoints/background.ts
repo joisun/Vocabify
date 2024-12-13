42b26c9554a6e7b7de826b3c783ce868614f9b3a
joi@rog <joi-sun@outlook.com>
2024-12-13 18:04:11 +0800

    fix: new Records re-render problem

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -2,73 +2,93 @@
  * https://stackoverflow.com/questions/77734376/indexeddb-not-showing-in-chrome-dev-tools#comment137042151_77734376
  * 在 插件的 Service Workder 中看不到 IndexedDB, 可以在 side panel 的控制台中看到
  */
-import VocabifyIndexDB from "@/lib/db";
-import { AiApiAdaptor } from "../lib/aiModels";
-import { firstSelection } from "@/utils/storage";
+import VocabifyIndexDB from '@/lib/db'
+import { AiApiAdaptor } from '../lib/aiModels'
+import { firstSelection } from '@/utils/storage'
 
 export default defineBackground(() => {
-  console.log("Hello background!", { id: browser.runtime.id });
-  chrome.sidePanel
-    .setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error) => console.error(error));
+    console.log('Hello background!', { id: browser.runtime.id })
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error))
 
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.create({ url: "https://react.dev/learn/render-and-commit" });
-  });
+    chrome.runtime.onInstalled.addListener(() => {
+        chrome.tabs.create({ url: 'https://react.dev/learn/render-and-commit' })
+    })
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const { payload } = message;
-    const MessageHandler = {
-      saveWordOrPhrase: async () => {
-        try {
-          const db = new VocabifyIndexDB();
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        const { payload } = message
+        const MessageHandler = {
+            saveWordOrPhrase: async () => {
+                try {
+                    const db = new VocabifyIndexDB()
 
-          db.addOrUpdateData(payload)
-            .then((res) => {
-              sendResponse({
-                status: "success",
-                message: res,
-              });
-            })
-            .catch((error) => {
-              sendResponse({
-                status: "error",
-                message: error,
-              });
-            });
-        } catch (err) {
-          sendResponse({
-            status: "error",
-            message:
-              "Vocabify Data Base Init error. Please Contact the developer.",
-          });
+                    db.addOrUpdateData(payload)
+                        .then((res) => {
+                            sendResponse({
+                                status: 'success',
+                                message: res,
+                            })
+                        })
+                        .catch((error) => {
+                            sendResponse({
+                                status: 'error',
+                                message: error,
+                            })
+                        })
+                } catch (err) {
+                    sendResponse({
+                        status: 'error',
+                        message: 'Vocabify Data Base Init error. Please Contact the developer.',
+                    })
+                }
+            },
+            findByPage: async () => {
+                try {
+                    const db = new VocabifyIndexDB()
+                    const { pageNum, pageSize } = payload
+                    db.findByPage(pageNum, pageSize)
+                        .then((res) => {
+                            sendResponse({
+                                status: 'success',
+                                message: res,
+                            })
+                        })
+                        .catch((error) => {
+                            sendResponse({
+                                status: 'error',
+                                message: error,
+                            })
+                        })
+                } catch (err) {
+                    sendResponse({
+                        status: 'error',
+                        message: 'Vocabify Data Base Init error. Please Contact the developer.',
+                    })
+                }
+            },
+
+            openSidePanel: async () => {
+                if (!sender.tab) return
+                await chrome.sidePanel.open({
+                    tabId: sender.tab.id,
+                    windowId: sender.tab.windowId,
+                })
+            },
+            triggerSelection: async () => {
+                await MessageHandler.openSidePanel()
+
+                /**
+                 * 当用户首次选中文本，而没有事先打开 side panel 的时候，sidepanel 内的消息监听还没有初始化， 所以首次 消息是监听不到的。
+                 * 为了解决这个问题，将用户首次选中的词汇缓存下来。 待 side panel 首次挂载的时候， 在  side panle 中取出执行。
+                 */
+                firstSelection.setValue(payload)
+                chrome.runtime.sendMessage({ action: 'sendToAi', payload })
+            },
         }
-      },
 
-      openSidePanel: async () => {
-        if (!sender.tab) return;
-        await chrome.sidePanel.open({
-          tabId: sender.tab.id,
-          windowId: sender.tab.windowId,
-        });
-      },
-      triggerSelection: async () => {
-        await MessageHandler.openSidePanel();
+        const action = message.action as keyof typeof MessageHandler
+        MessageHandler[action] && MessageHandler[action]()
 
-        /**
-         * 当用户首次选中文本，而没有事先打开 side panel 的时候，sidepanel 内的消息监听还没有初始化， 所以首次 消息是监听不到的。
-         * 为了解决这个问题，将用户首次选中的词汇缓存下来。 待 side panel 首次挂载的时候， 在  side panle 中取出执行。
-         */
-        firstSelection.setValue(payload);
-        chrome.runtime.sendMessage({ action: "sendToAi", payload });
-      },
-    };
-
-    const action = message.action as keyof typeof MessageHandler;
-    MessageHandler[action] && MessageHandler[action]();
-
-
-    // https://developer.chrome.com/docs/extensions/develop/concepts/messaging#simple
-    return true;
-  });
-});
+        // https://developer.chrome.com/docs/extensions/develop/concepts/messaging#simple
+        return true
+    })
+})
--- entrypoints/sidepanel/components/Layout.tsx
+++ entrypoints/sidepanel/components/Layout.tsx
@@ -1,77 +1,60 @@
-import { Button } from "@/components/ui/button"
-import { Settings } from "lucide-react";
-
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-} from "@/components/ui/tabs"
-import { isValidElement } from "react";
+import { Button } from '@/components/ui/button'
+import { Settings } from 'lucide-react'
+import { Provider, KeepAlive } from 'react-keep-alive'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { isValidElement } from 'react'
 
 export const Layout = ({ children }: { children: React.ReactElement[] }) => {
-    type SubPanelType = 'NewRecordPanel' | 'RecordsPanel'
-    const panels = [...children].reduce((acc: { [x: string]: React.ReactElement }, child) => {
-        if (isValidElement(child)) {
-            const { type } = child;
-            if (typeof type === 'function') {
-                acc[type.name] = child; // 将各个子组件分类存储
-            }
-        }
-        return acc;
-    }, {}) as { [key in SubPanelType]: React.ReactElement };
-
-    const handleClickSetting = ()=>{
-        chrome.runtime.openOptionsPage()
+  type SubPanelType = 'NewRecordPanel' | 'RecordsPanel'
+  const panels = [...children].reduce((acc: { [x: string]: React.ReactElement }, child) => {
+    if (isValidElement(child)) {
+      const { type } = child
+      if (typeof type === 'function') {
+        acc[type.name] = child // 将各个子组件分类存储
+      }
     }
-    return (
-        <div className="flex m-2">
-            <Tabs defaultValue="newrecord" className="w-full">
-                <TabsList className="w-full flex justify-between">
-                    <div>
-                        <TabsTrigger value="newrecord">New Record</TabsTrigger>
-                        <TabsTrigger value="records">Records</TabsTrigger>
-                    </div>
-                    <Button variant={"ghost"} onClick={handleClickSetting}>
-                        <Settings />
-                    </Button>
+    return acc
+  }, {}) as { [key in SubPanelType]: React.ReactElement }
 
-                </TabsList>
-                <TabsContent value="newrecord">
-                    <Card>
-                        <CardContent className="p-2">
-                            {panels.NewRecordPanel}
-                        </CardContent>
-                    </Card>
-                </TabsContent>
-                <TabsContent value="records">
-                    <Card>
-                        <CardContent className="p-2">
-                            {panels.RecordsPanel}
-                        </CardContent>
-                    </Card>
-                </TabsContent>
-            </Tabs>
-        </div>
-    )
+  const handleClickSetting = () => {
+    chrome.runtime.openOptionsPage()
+  }
+
+  const [activeTab, setActiveTab] = useState('newrecord')
+  return (
+    <div className="flex m-2">
+      <Tabs defaultValue="newrecord" className="w-full" value={activeTab} onValueChange={setActiveTab}>
+        <TabsList className="w-full flex justify-between">
+          <div>
+            <TabsTrigger value="newrecord">New Record</TabsTrigger>
+            <TabsTrigger value="records">Records</TabsTrigger>
+          </div>
+          <Button variant={'ghost'} onClick={handleClickSetting}>
+            <Settings />
+          </Button>
+        </TabsList>
+        {/* https://github.com/radix-ui/primitives/issues/1155#issuecomment-1712307236 解决tabs切换 re-render 问题 */}
+        <TabsContent value="newrecord" forceMount hidden={activeTab !== 'newrecord'}>
+          <Card>
+            <CardContent className="p-2">{panels.NewRecordPanel}</CardContent>
+          </Card>
+        </TabsContent>
+        {/* 如果不指定 forceMount 那么就会重新渲染*/}
+        <TabsContent value="records" hidden={activeTab !== 'records'}>
+          <Card>
+            <CardContent className="p-2">{panels.RecordsPanel}</CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 }
 
 // NewRecordPanel 组件
-export const NewRecordPanel = ({ children }: { children?: React.ReactElement }) => (
-    <>{children}</>
-);
+export const NewRecordPanel = ({ children }: { children?: React.ReactElement }) => <>{children}</>
 
 // RecordsPanel 组件
-export const RecordsPanel = ({ children }: { children?: React.ReactElement }) => (
-    <>{children}</>
-);
+export const RecordsPanel = ({ children }: { children?: React.ReactElement }) => <>{children}</>
