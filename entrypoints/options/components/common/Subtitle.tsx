0942c1f575127cb62a21244d97f297e1a8c64c58
joi@rog <joi-sun@outlook.com>
2024-12-22 14:03:41 +0800

    feat: optimize prompt template

--- entrypoints/options/components/common/Subtitle.tsx
+++ entrypoints/options/components/common/Subtitle.tsx
@@ -1,5 +1,5 @@
 export default function Subtitle({ children }: { children: React.ReactNode }) {
     return (
-        <h3 className="text-xs text-left w-fit text-white/50 px-1 rounded-lg font-thin mb-2">{children}</h3>
+        <h3 className="text-xs text-left w-fit text-foreground/80 px-1 rounded-lg font-thin mb-2">{children}</h3>
     )
 }
